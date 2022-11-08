22479fdace5f6392c0f9dfccc30e2921071c65eb
Gerson Vasquez <averienglish@aol.com>
2022-11-08 17:10:27 -0600

    clear HTML when countdown is 0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 var interval = 1000;
+var timeAnnouncement = document.getElementById('time-announcement');
+let timerId
 
 const countdown =()=>{
-    const countDate = new Date("December 25, 2022 00:00:00").getTime();
+    const countDate = new Date("November 8, 2022 15:26:00").getTime();
     // console.log(countDate)
     const now = new Date().getTime();
     const gap = countDate - now;
@@ -23,10 +25,35 @@ const countdown =()=>{
     document.querySelector(".minute").innerText = textMinute;
     document.querySelector(".second").innerText = textSecond;
 
+    //what if christmas has passed
+    if (gap <= -textDay) {
+        timeAnnouncement.innerHTML = "Hope you all had a great day";
+        document.querySelector(".day").innerText = "";
+        document.querySelector(".day-text").innerHTML = "";
+        document.querySelector(".hour").innerText = "";
+        document.querySelector(".hour-text").innerHTML="";
+        document.querySelector(".minute").innerText = "";
+        document.querySelector(".minute-text").innerHTML="";
+        document.querySelector(".second").innerText = "";
+        document.querySelector(".second-text").innerHTML="";
+        clearInterval(timerId);
+        return
+    }
+
     //thinking about when the countdown reaches zero
-    // if (gap <= 0) {
-    //     clearInterval(countInterval);
-    // }   
+    if (gap <= second) {
+        timeAnnouncement.innerHTML = "Time is up";
+        document.querySelector(".day").innerText = "";
+        document.querySelector(".day-text").innerHTML = "";
+        document.querySelector(".hour").innerText = "";
+        document.querySelector(".hour-text").innerHTML="";
+        document.querySelector(".minute").innerText = "";
+        document.querySelector(".minute-text").innerHTML="";
+        document.querySelector(".second").innerText = "";
+        document.querySelector(".second-text").innerHTML="";
+        clearInterval(timerId);
+        return
+    }   
 };
 
-setInterval(countdown, interval);
+timerId=setInterval(countdown, interval);
